71294d39f9118ead002c9990f1daa78e2c5f468c
MMomcilovic <miladin.momcilovic@gmail.com>
2024-08-25 00:48:16 +0200

    feat: project initialization with cicd

---
+++ src/test/java/com/devops/rating_service/RatingServiceApplicationTests.java
@@ -0,0 +1,23 @@
+package com.devops.rating_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@ExtendWith(MockitoExtension.class)
+class RatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@BeforeEach
+	void setUp() {
+
+	}
+
+	@Test
+	void testGetAccomodation() {
+
+	}
+
+}
