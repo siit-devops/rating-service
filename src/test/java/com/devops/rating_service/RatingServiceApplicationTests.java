25b391deac7e6f478c1a33b8f6f06577373f1b8c
MMomcilovic <61472289+MMomcilovic@users.noreply.github.com>
2024-08-25 12:51:16 +0200

    feat: project initialization with cicd (#1)
    * feat: project initialization with cicd
    
    * fix: test imports

---
+++ src/test/java/com/devops/rating_service/RatingServiceApplicationTests.java
@@ -0,0 +1,27 @@
+package com.devops.rating_service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class RatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@BeforeEach
+	void setUp() {
+
+	}
+
+	@Test
+	void testGetAccomodation() {
+
+	}
+
+}
